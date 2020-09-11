fb1f31a91614d2145866db9b184a3cfd1f93aa2b
Muhammet Ali ERŞEN <muhammetaliersen@gmail.com>
2020-09-11 15:13:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Projenin 2 kısmı yani Bitcoin fiyarlarının gösterildiği kısmı apiden veri alamadığım için tamamlayamadım. Apiden hiç bir şekilde veriyi çekemedim.
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+
---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Timer from 'react-compound-timer'
+
+
+const withTimer = timerProps => WrappedComponent => wrappedComponentProps => (
+    <Timer {...timerProps}>
+        {timerRenderProps =>
+            <WrappedComponent {...wrappedComponentProps} timer={timerRenderProps} />}
+    </Timer>
+);
+
+
+
+class TimeUpDown extends React.Component {
+
+    render() {
+        const { start, setTime, getTime } = this.props.timer; 
+        return (
+            <div>
+                <div style={{ display: 'flex' }}>
+                    <h1>
+                        <Timer.Hours />:
+                        <Timer.Minutes />:
+                        <Timer.Seconds />
+                    </h1>
+                </div>
+                <div style={{ display: 'table', margin: 'auto' }}>
+                    <button class={'MyButton'} onClick={() => { setTime(getTime() + 60 * 60000); start(); }}>+</button>
+                    <button class={'MyButton'} onClick={() => { setTime(getTime() + 60000); start(); }}>+</button>
+                    <button onClick={() => { setTime(getTime() + 1000); start(); }}>+</button>
+
+                </div>
+                <div class={'divtop'} style={{ display: 'table', margin: 'auto' }}>
+                    <button class={'MyButton'} onClick={() => { getTime() - 60 * 60000 > 0 ? setTime(getTime() - 60 * 60000) : setTime(0) }}>-</button>
+                    <button class={'MyButton'} onClick={() => { getTime() - 60000 > 0 ? setTime(getTime() - 60000) : setTime(0) }}>-</button>
+                    <button onClick={() => { getTime() - 1000 > 0 ? setTime(getTime() - 1000) : setTime(0) }}>-</button>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+const TimerHOC = withTimer({
+    direction: 'backward',
+    initialTime: 60000 * 60 * 10,
+    lastUnit: "h",
+
+})(TimeUpDown);
+
+ReactDOM.render(
+    <TimerHOC />,
+    document.getElementById('root')
+);
